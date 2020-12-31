9444f5bacc933658d20a03a19a11bcfd7cd72a6e
brenonoccioli <brenonoccioli@gmail.com>
2020-12-30 22:02:25 -0300

    estudos sobre interface e composição em Java

---
+++ bytebank-herdado-conta/src/Tributavel.java
@@ -0,0 +1,6 @@
+//não necessita definir como abstract pois a interface já é abstrata por definição
+public interface Tributavel {
+	//também não necessita definir os métodos como public abstract pois essa é a definição da interace
+	double getValorImposto();
+	
+}
