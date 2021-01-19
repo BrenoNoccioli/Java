df6304b3d72369a1b43956579c9b42e5e13ea4b1
brenonoccioli <brenonoccioli@gmail.com>
2021-01-19 10:24:12 -0300

    hello world com spring boot

---
+++ spring/spring-boot/forum/src/test/java/br/com/projetos/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
